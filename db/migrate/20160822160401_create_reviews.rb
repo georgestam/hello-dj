d6fe8161848428fce2a084fa9abcea8dd06538a1
Dave Anderson <dave@fastforwardtime.co.uk>
2016-08-22 17:11:39 +0100

    changed rating of review model to integer

--- db/migrate/20160822160401_create_reviews.rb
+++ db/migrate/20160822160401_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
-      t.string :rating
+      t.integer :rating
       t.string :description
 
       t.timestamps
