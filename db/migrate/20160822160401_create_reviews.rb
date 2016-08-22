c7452ad08eab9f4e89436f3a04b528ffecd0232c
Dave Anderson <dave@fastforwardtime.co.uk>
2016-08-22 18:17:57 +0100

    added missing columns

--- db/migrate/20160822160401_create_reviews.rb
+++ db/migrate/20160822160401_create_reviews.rb
@@ -1,6 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
+      t.references :user, foreign_key: true
+      t.references :booking, foreign_key: true
       t.integer :rating
       t.string :description
 
