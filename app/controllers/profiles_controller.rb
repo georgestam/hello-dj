cca7df7ea4e08689a48cc751fb8d7d6855a5640c
Dave Anderson <dave@fastforwardtime.co.uk>
2016-08-23 17:45:51 +0100

    Basic Dashboard Done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,11 +14,13 @@ before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
   def create
     @profile = Profile.new(profile_params)
+    @profile.user = current_user
     if @profile.save
       redirect_to profile_path(@profile)
     else
       render :new
     end
+    # @profile.user = current_user
   end
 
   def edit
@@ -40,6 +42,9 @@ private
   end
 
   def profile_params
-    params.require(:profile).permit(:dj_name, :soundcloud_link, :price_hour)
+    params.require(:profile).permit(:dj_name, :soundcloud_link, :price_hour, :bio)
   end
 end
+
+
+
