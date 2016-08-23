d6ada255fc6d08d6ac35870fc1d250c522e68664
Dave Anderson <dave@fastforwardtime.co.uk>
2016-08-23 10:30:04 +0100

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create, :edit, :update, :destroy] do
+    resources :dashboards, only: [:show]
+  end
+
+  resources :bookings, only: [:new, :create, :show, :destroy] do
+    resources :dashboards, only: [:new, :create, :show]
+  end
+
+  resources :djs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
