bc416338f96b1fb11b1f4d423f2e723a0a5bb3d2
jimmy littmann <littmann.j@gmail.com>
2016-08-23 16:50:34 +0100

    added reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,26 @@
 class ReviewsController < ApplicationController
 
-  def show
-    @review = Review.find(params[:id])
-  end
+  before_action :set_booking
 
   def new
-    @review = Review.new(
+    @review = @booking.reviews.build
   end
 
   def create
-    @booking.find(params[:booking_id])
-    @review = Review.new(review_params)
-    if @review.save
-      redirect_to booking_path(@booking)
-    else
-      render :new
-    end
+    review = Review.new(review_params)
+    @review.booking = @booking
+    @review.save
+    redirect_to booking_path(@booking)
   end
 
-end
+  private
 
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
 
-def review_params
-  params.require(:review).permit(:description, :rating)
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
 
 belongs_to :booking
-belongs_to :user
 
 validates :rating,  inclusion: { in: [1...5], allow_nil: false }
 validates :user_id, presence: true
