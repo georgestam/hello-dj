42af7f67d1a602e001a4139a82598259bb3a7cd8
jimmy littmann <littmann.j@gmail.com>
2016-08-23 16:16:08 +0100

    booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,46 @@
 class BookingsController < ApplicationController
 
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
 
   def new
+    @booking = Booking.new
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def destroy
+    @booking.destroy
   end
 
+private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :booking_address, :time)
+  end
 end
