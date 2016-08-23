560ffa9a2f9895fc9285ea281f8f6c307b5094dc
Dave Anderson <dave@fastforwardtime.co.uk>
2016-08-23 13:10:41 +0100

    Added review model migration and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,26 @@
 class ReviewsController < ApplicationController
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def new
+    @review = Review.new(
   end
 
   def create
+    @booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
+end
+
+
+def cocktail_params
+  params.require(:review).permit(:description, :rating)
 end
