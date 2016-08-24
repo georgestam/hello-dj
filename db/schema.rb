620d5aa8a9c3d1d7ce6e647279158baa7d5088e0
jimmy littmann <littmann.j@gmail.com>
2016-08-24 10:42:21 +0100

    added picture to profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823120517) do
+ActiveRecord::Schema.define(version: 20160824093101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160823120517) do
     t.integer  "price_hour"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "picture"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
