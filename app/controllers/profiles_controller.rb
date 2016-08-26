c0e8ac5729eb16e40db262fc8f854fa5f2ccb7d7
Jordi Fernandez <jordi@ywarp.com>
2016-08-26 13:39:41 +0100

    reviews

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -40,6 +40,12 @@ class ProfilesController < ApplicationController
 
     # .slice(0).slice(-1)
 
+    @reviews = Review.all
+    # @reviews = Review.where(profile: @profile.bookings)
+
+    # Booking.where(profile: current_user.profiles)
+
+    # @popular_profiles = Profile.where.not(photo: nil).order(created_at: :asc).limit(6).reverse
 
   end
 
