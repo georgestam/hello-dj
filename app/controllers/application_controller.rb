efa703fe91f61ceba4e233f6ca61bedbea833af8
Dave Anderson <dave@fastforwardtime.co.uk>
2016-08-26 16:29:30 +0100

    added address field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :photo_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :photo, :photo_cache])
   end
 end
